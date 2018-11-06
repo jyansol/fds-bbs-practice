1358b410bb95483f217df9b9f2f6ea924ed35567
jyansol <hahn0406@gmail.com>
2018-11-06 15:44:29 +0900

    게시물목록표시 기능

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
-import '@babel/polyfill'
+import '@babel/polyfill';
 
-import axios from 'axios'
+import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.API_URL
-})
+  //바깥에서 주입해준 환경변수를 사용하는 코드
+  //이 컴퓨터에서만 사용할 환경변수를 설정하기 우해새 .env 파일을 편집
+  //.env 파일에는 개발용 서버 주소를 넣어줌 => 서버를 껐다켜야함
+  baseURL: process.env.API_URL,
+});
 
-api.interceptors.request.use(function (config) {
-  const token = localStorage.getItem('token')
+api.interceptors.request.use(function(config) {
+  const token = localStorage.getItem('token');
   if (token) {
-    config.headers = config.headers || {}
-    config.headers['Authorization'] = 'Bearer ' + token
+    config.headers = config.headers || {};
+    config.headers['Authorization'] = 'Bearer ' + token;
   }
-  return config
+  return config;
 });
 
 const templates = {
@@ -22,9 +25,9 @@ const templates = {
   postForm: document.querySelector('#post-form').content,
   postDetail: document.querySelector('#post-detail').content,
   commentItem: document.querySelector('#comment-item').content,
-}
+};
 
-const rootEl = document.querySelector('.root')
+const rootEl = document.querySelector('.root');
 
 // 페이지 그리는 함수 작성 순서
 // 1. 템플릿 복사
@@ -36,47 +39,71 @@ const rootEl = document.querySelector('.root')
 
 async function drawLoginForm() {
   // 1. 템플릿 복사
-  const frag = document.importNode(templates.loginForm, true)
+  const frag = document.importNode(templates.loginForm, true);
 
   // 2. 요소 선택
-  const formEl = frag.querySelector('.login-form')
+  const formEl = frag.querySelector('.login-form');
 
   // 3. 필요한 데이터 불러오기 - 필요없음
   // 4. 내용 채우기 - 필요없음
   // 5. 이벤트 리스너 등록하기
-  formEl.addEventListener('submit', async e => {
-    e.preventDefault()
-    const username = e.target.elements.username.value
-    const password = e.target.elements.password.value
+  formEl.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const username = e.target.elements.username.value;
+    const password = e.target.elements.password.value;
 
     const res = await api.post('/users/login', {
       username,
-      password
-    })
+      password,
+    });
 
-    localStorage.setItem('token', res.data.token)
-    drawPostList()
-  })
+    localStorage.setItem('token', res.data.token);
+    drawPostList();
+  });
 
   // 6. 템플릿을 문서에 삽입
-  rootEl.textContent = ''
-  rootEl.appendChild(frag)
+  rootEl.textContent = '';
+  rootEl.appendChild(frag);
 }
 
 async function drawPostList() {
   // 1. 템플릿 복사
-  const frag = document.importNode(templates.postList, true)
+  const frag = document.importNode(templates.postList, true);
 
   // 2. 요소 선택
+  const listEl = frag.querySelector('.post-list');
   // 3. 필요한 데이터 불러오기
+  // 분해대입, data라는 속성을 꺼내와서 미리 대입해 줄 수 있음
+  //data라는 속성값을 불러와서 postList를 저장; const postList = res.data
+  const { data: postList } = await api.get('/posts?_expand=user');
   // 4. 내용 채우기
+  for (const postItem of postList) {
+    const frag = document.importNode(templates.postItem, true);
+    const idEl = frag.querySelector('.id');
+    const titleEl = frag.querySelector('.title');
+    const authorEl = frag.querySelector('.author');
+
+    idEl.textContent = postItem.id;
+    titleEl.textContent = postItem.title;
+    authorEl.textContent = postItem.user.username;
+
+    listEl.appendChild(frag);
+
+    titleEl.addEventListener('click', (e) => {
+      console.log(postItem.id);
+      //클릭한 놈의 아이디
+      drawPostDetail(postItem.id);
+    });
+  }
   // 5. 이벤트 리스너 등록하기
 
   // 6. 템플릿을 문서에 삽입
-  rootEl.textContent = ''
-  rootEl.appendChild(frag)
+  rootEl.textContent = '';
+  rootEl.appendChild(frag);
 }
 
+//게시물을 그리는 함수
+//postId를 매개변수로 받고 있음 -> 어떤 게시물을 그릴지 알려줘야하니까
 async function drawPostDetail(postId) {
   // 1. 템플릿 복사
   // 2. 요소 선택
@@ -106,7 +133,7 @@ async function drawEditPostForm(postId) {
 
 // 페이지 로드 시 그릴 화면 설정
 if (localStorage.getItem('token')) {
-  drawPostList()
+  drawPostList();
 } else {
-  drawLoginForm()
+  drawLoginForm();
 }
