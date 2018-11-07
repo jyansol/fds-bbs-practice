b4d4872b7f995dbdb738f3d3928b3c34e0e45b0b
jyansol <hahn0406@gmail.com>
2018-11-07 12:46:24 +0900

    댓글기능추가(버그있음)

--- src/index.js
+++ src/index.js
@@ -119,6 +119,7 @@ async function drawPostDetail(postId) {
   const backEl = frag.querySelector('.back');
   const commentListEl = frag.querySelector('.comment-list');
   const commentFormEl = frag.querySelector('.comment-form');
+  const updateEl = frag.querySelector('.update');
 
   // 3. 필요한 데이터 불러오기
   // data title,body
@@ -162,6 +163,7 @@ async function drawPostDetail(postId) {
     authorEl.textContent = user.username;
 
     // 5. 이벤트 리스너 등록하기
+    // 오류있음
     commentFormEl.addEventListener('submit', async (e) => {
       e.preventDefault();
       const body = e.target.elements.body.value;
@@ -179,6 +181,10 @@ async function drawPostDetail(postId) {
   backEl.addEventListener('click', (e) => {
     // 목록을 다시 그려주는 페이지를 출력해야하니까
     drawPostList();
+  });
+  //수정
+  updateEl.addEventListener('click', (e) => {
+    drawEditPostForm(postId);
   });
   // 6. 템플릿을 문서에 삽입
   rootEl.textContent = '';
@@ -216,11 +222,37 @@ async function drawNewPostForm() {
 
 async function drawEditPostForm(postId) {
   // 1. 템플릿 복사
+  const frag = document.importNode(templates.postForm, true);
   // 2. 요소 선택
+  const formEl = frag.querySelector('.post-form');
+  const backEl = frag.querySelector('.back');
+  const titleEl = frag.querySelector('.title');
+  const bodyEl = frag.querySelector('.body');
   // 3. 필요한 데이터 불러오기
+  const {
+    data: { title, body },
+  } = await api.get('/posts/' + postId);
   // 4. 내용 채우기
+  titleEl.value = title;
+  bodyEl.value = body;
   // 5. 이벤트 리스너 등록하기
+  formEl.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const title = e.target.elements.title.value;
+    const body = e.target.elements.body.value;
+    await api.patch('/posts/' + postId, {
+      title,
+      body,
+    });
+    drawPostList();
+  });
+  backEl.addEventListener('click', (e) => {
+    e.preventDefault();
+    drawPostList();
+  });
   // 6. 템플릿을 문서에 삽입
+  rootEl.textContent = '';
+  rootEl.appendChild(frag);
 }
 
 // 페이지 로드 시 그릴 화면 설정
